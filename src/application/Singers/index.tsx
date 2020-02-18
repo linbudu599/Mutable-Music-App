ec31f7fbc53eb1ab4892d56baff2394ebbd88653
linbudu599 <linbudu599@gmail.com>
2020-02-18 10:18:10 +0800

    :sparkles: feat: Singer Banner

--- src/application/Singers/index.tsx
+++ src/application/Singers/index.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import Horizen from "../../baseUI/horizon-item";
+import { categoryTypes, alphaTypes } from "../../api/config";
+import { NavContainer } from "./style";
 
-const Singer: React.FC = (props: any) => {
-  return <div>Singer</div>;
+const Singers: React.FC = () => {
+  let [category, setCategory] = useState<string>("");
+  let [alpha, setAlpha] = useState<string>("");
+
+  let handleUpdateAlpha = (val: string): void => {
+    setAlpha(val);
+  };
+
+  let handleUpdateCatetory = (val: string): void => {
+    setCategory(val);
+  };
+  return (
+    <NavContainer>
+      <Horizen
+        list={categoryTypes}
+        title={"分类 (默认热门):"}
+        handleClick={val => handleUpdateCatetory(val)}
+        oldVal={category}
+      ></Horizen>
+      <Horizen
+        list={alphaTypes}
+        title={"首字母:"}
+        handleClick={val => handleUpdateAlpha(val)}
+        oldVal={alpha}
+      ></Horizen>
+    </NavContainer>
+  );
 };
 
-export default React.memo(Singer);
+export default React.memo(Singers);
--- src/baseUI/horizon-item/index.tsx
+++ src/baseUI/horizon-item/index.tsx
@@ -1,22 +1,33 @@
-import React, { useState, useRef, useEffect, memo } from "react";
+import React, { useState, useRef, useEffect, memo, DOMElement } from "react";
 import styled from "styled-components";
 import Scroll from "../scroll";
 import { List, ListItem } from "./style";
 
 interface IHorizon {
   list: any[];
-  oldVal: string;
-  title: string;
-  handleClick: (key: number) => void;
+  oldVal?: string;
+  title?: string;
+  handleClick: (key: string) => void;
 }
 
-type IH = Partial<IHorizon>;
-
 const Horizen: React.FC<IHorizon> = ({ list, oldVal, title, handleClick }) => {
+  const Category = useRef(null);
+
+  useEffect(() => {
+    let categoryDOM = (Category.current as unknown) as HTMLElement;
+    let tagElems = categoryDOM.querySelectorAll("span");
+    let totalWidth = 0;
+    Array.from(tagElems).forEach((ele: any) => {
+      totalWidth += ele.offsetWidth;
+    });
+    // 动态生成宽度
+    categoryDOM.style.width = `${totalWidth}px`;
+  }, [Category]);
+
   return (
     <>
       <Scroll direction={"horizental"}>
-        <div>
+        <div ref={Category}>
           <List>
             <span>{title}</span>
             {list.map(item => {
@@ -43,3 +54,5 @@ Horizen.defaultProps = {
   title: "",
   handleClick: () => {}
 };
+
+export default Horizen;
